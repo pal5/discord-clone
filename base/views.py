58fb0f0ef1be0143f44c1907207e7a9c78073822
Prabhath <prabhath.paluru@oracle.com>
2023-11-25 13:21:06 +0530

    feat: using context for passing data to templates

--- base/views.py
+++ base/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name': "Learning BufferOverFlow"},
+    {'id':2, 'name': "Front End Dev"},
+    {'id':3, 'name': "Self Help Group"},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context=context)
 
 def room(request):
     return render(request, 'room.html')
