868bf782a6f554e9ceadf753273e11165984c312
Prabhath <p.prabhath52000@gmail.com>
2023-11-27 14:37:34 +0530

    feat: support for creating new rooms using form

--- base/views.py
+++ base/views.py
@@ -12,3 +12,7 @@ def room(request, id:str):
     room = Room.objects.get(id=id)
     context = {'room': room}
     return render(request, 'base/room.html', context=context)
+
+def create_room(request):
+    context = {}
+    return render(request, 'base/create-room.html', context=context)
