bce6d3f70fb332c366d5ddd91cb7091a24e75eda
Prabhath <p.prabhath52000@gmail.com>
2023-11-29 16:06:07 +0530

    feat: support for querying on mulitple conditions and display room count

--- base/views.py
+++ base/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Q
 
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ""
-    rooms = Room.objects.filter(topic__name__icontains=q) # ModelName.objects => model manager to query from the database
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)) # ModelName.objects => model manager to query from the database
     topics = Topic.objects.all()
-    context = {'rooms':rooms,'topics':topics}
+    rooms_count = rooms.count()
+    context = {'rooms':rooms,'topics':topics,'rooms_count':rooms_count}
     return render(request, 'base/home.html', context=context)
 
 def room(request, id:str):
